4da2e2b81906c41005fd9163730ec4e5e40c9aac
TheDandyBebop <Sanzerieric@gmail.com>
2023-04-11 19:39:36 +0200

    Confirmo la clase de la rama1

---
+++ Ejemplo2/src/clase_2_Rama1.java
@@ -0,0 +1,11 @@
+
+public class clase_2_Rama1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("primer mensaje Rama 1");
+		System.out.println("Segundo mensaje Rama 1");
+
+	}
+
+}
